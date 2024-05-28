7847b4d3bcec5afd4d601abfed867ba2c1389c4f
A Lotov <alilotfollahi2000@gmail.com>
2024-05-28 17:05:37 +0330

    refactor: DeleteOrderRq has been refactored so that it represents a value object + it must now be constructed with its builder and other constructors are now annotated with @Deprecated

--- src/main/java/ir/ramtung/tinyme/messaging/request/DeleteOrderRq.java
+++ src/main/java/ir/ramtung/tinyme/messaging/request/DeleteOrderRq.java
@@ -1,25 +1,30 @@
 package ir.ramtung.tinyme.messaging.request;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import ir.ramtung.tinyme.domain.entity.Side;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Value;
 
 import java.time.LocalDateTime;
 
-@EqualsAndHashCode(callSuper = false)
-@Data
-public class DeleteOrderRq extends Request {
-    private String securityIsin;
-    private Side side;
-    private long orderId;
+@Value
+@Builder
+@AllArgsConstructor(onConstructor = @__({@Deprecated}))
+public class DeleteOrderRq {
+    long requestId;
+    String securityIsin;
+    Side side;
+    long orderId;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @Builder.Default
+    LocalDateTime entryTime = LocalDateTime.now();
 
-    public DeleteOrderRq(long requestId, String securityIsin, Side side, long orderId, LocalDateTime entryTime) {
-        super(requestId, entryTime);
-        this.securityIsin = securityIsin;
-        this.side = side;
-        this.orderId = orderId;
-    }
-
+    @Deprecated
     public DeleteOrderRq(long requestId, String securityIsin, Side side, long orderId) {
         this(requestId, securityIsin, side, orderId, LocalDateTime.now());
     }
