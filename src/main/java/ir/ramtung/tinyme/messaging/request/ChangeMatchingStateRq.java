927892a3d33fd314b982e546463f2b26cf2a910d
A Lotov <alilotfollahi2000@gmail.com>
2024-05-28 17:10:31 +0330

    refactor: ChangeMatchingStateRq has been refactored so that it represents a value object + it must now be constructed with its builder and other constructors are now annotated with @Deprecated + Request.java was deleted

--- src/main/java/ir/ramtung/tinyme/messaging/request/ChangeMatchingStateRq.java
+++ src/main/java/ir/ramtung/tinyme/messaging/request/ChangeMatchingStateRq.java
@@ -1,21 +1,25 @@
 package ir.ramtung.tinyme.messaging.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Value;
 
 import java.time.LocalDateTime;
 
-@Data
-@EqualsAndHashCode(callSuper = false)
-public class ChangeMatchingStateRq extends Request {
+@Value
+@Builder
+@AllArgsConstructor(onConstructor = @__({@Deprecated}))
+public class ChangeMatchingStateRq {
+    long requestId;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    LocalDateTime entryTime;
     String securityIsin;
     @JsonFormat(shape = JsonFormat.Shape.STRING)
     MatchingState targetState;
-
-    public ChangeMatchingStateRq(long requestId, LocalDateTime entryTime, String securityIsin, MatchingState targetState) {
-        super(requestId, entryTime);
-        this.securityIsin =securityIsin;
-        this.targetState = targetState;
-    }
 }
