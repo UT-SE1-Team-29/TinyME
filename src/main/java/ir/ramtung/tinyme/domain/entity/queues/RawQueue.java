e7c1b423a7e5ddff0b3ea17dabb738572cb45e64
A Lotov <alilotfollahi2000@gmail.com>
2024-05-30 04:44:34 +0330

    refactor: unused queue was deleted

--- src/main/java/ir/ramtung/tinyme/domain/entity/queues/RawQueue.java
+++
@@ -1,8 +0,0 @@
-package ir.ramtung.tinyme.domain.entity.queues;
-
-import java.util.LinkedList;
-import ir.ramtung.tinyme.domain.entity.order.Order;
-
-
-public class RawQueue extends LinkedList<Order> implements Queue {
-}
